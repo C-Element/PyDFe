e60af6b0073cd04ccf0ed9a97ff3096c8f86a6e4
Clemente Junior <clmnt.jr@gmail.com>
2017-09-29 17:38:41 -0300

    ADD: Função para formatar moeda >>> f_moeda

--- documento/formatos.py
+++ documento/formatos.py
@@ -1,4 +1,5 @@
 from datetime import date
+from decimal import Decimal
 
 _data_dma = '%d/%m/%Y'
 
@@ -6,3 +7,6 @@ _data_dma = '%d/%m/%Y'
 def f_dma(data: date):
     return data.strftime(_data_dma)
 
+
+def f_moeda(valor: Decimal):
+    return f'R$ {valor:,.2f}'
