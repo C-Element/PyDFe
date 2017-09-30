42226c31c8d68433a2b07b1d79e982afb6d2bb87
Clemente Junior <clmnt.jr@gmail.com>
2017-09-30 10:26:47 -0300

    ADD: formatar inteiros seprando-os por milhar

--- documento/formatos.py
+++ documento/formatos.py
@@ -10,3 +10,7 @@ def f_dma(data: date):
 
 def f_moeda(valor: Decimal):
     return f'R$ {valor:,.2f}'.replace('.', '*').replace(',', '.').replace('*', ',')
+
+
+def f_int_milhar(valor: int):
+    return f'{valor:,}'.replace(',', '.')
