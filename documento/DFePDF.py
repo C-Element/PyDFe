e98874aa0de03122e4abde4c2f13796113670ced
Clemente Junior <clmnt.jr@gmail.com>
2017-10-06 17:37:52 -0300

    ADD: Primeira versão do cabeçalho

--- documento/DFePDF.py
+++ documento/DFePDF.py
@@ -1,8 +1,22 @@
+from math import fmod
+
 from fpdf import FPDF
 
-SUPERIOR = 0
-DIREITA = 1
-ESQUERDA = 2
+DIREITA = 2
+ESQUERDA = 0
+SUPERIOR = 1
+
+
+def strtr(texto: str, de: str, para: str) -> str:
+    tradutor = dict(zip(list(de), list(para)))
+    texto_lista = list(texto)
+    i = 0
+    while i < len(texto_lista):
+        l = texto_lista[i]
+        if l in tradutor.keys():
+            texto_lista[i] = tradutor[l]
+        i += 1
+    return ''.join(texto_lista)
 
 
 class FontePDF(object):
@@ -13,17 +27,168 @@ class FontePDF(object):
 
 
 class DFePDF(FPDF):
-    def __init__(self, margens: list = (3, 3, 3)):
+    def __init__(self, margens: list = (7, 7, 7)):
         super().__init__()
         self.margens = margens  # Superior, Direita, Inferior, Esquerda
-        self.x = 1  # self.x e self.y onde iniciará a 'desenhar' na página
-        self.y = 1
-        self.max_altura = 297 - self.margens[SUPERIOR] + self.y
-        self.max_largura = 210 - self.margens[ESQUERDA] - self.margens[DIREITA] + self.x  # Iremos trabalhar a principio apenas com A4 em porta retrato
-
+        self.x = self.margens[ESQUERDA]  # self.x e self.y onde iniciará a 'desenhar' na página
+        self.y = self.margens[SUPERIOR]
+        self.altura_max = 297 - self.margens[SUPERIOR]
+        self.largura_max = 210 - self.margens[ESQUERDA]  # Iremos trabalhar a principio apenas com A4 em porta retrato
         self.set_margins(*self.margens)
-
         self.alias_nb_pages()
+
+        self.conjunto_abc = ''  # conjunto de caracteres legiveis em 128
+        self.conjunto_a = ''  # grupo A do conjunto de de caracteres legiveis
+        self.conjunto_b = ''  # grupo B do conjunto de caracteres legiveis
+        self.conjunto_c = ''  # grupo C do conjunto de caracteres legiveis
+        self.conjunto_de = dict(A='', B='', C='')  # converter de
+        self.conjunto_para = dict(A='', B='', C='')  # converter para
+        self.conjunto_inicio = dict(A=103, B=104, C=105)  # Caracteres de seleção do grupo 128
+        self.conjunto_troca = dict(A=101, B=100, C=99)  # Caracteres de troca de grupo
+        self.tabela_128 = [[2, 1, 2, 2, 2, 2],  # 0 : [ ]
+                           [2, 2, 2, 1, 2, 2],  # 1 : [!]
+                           [2, 2, 2, 2, 2, 1],  # 2 : ["]
+                           [1, 2, 1, 2, 2, 3],  # 3 : [#]
+                           [1, 2, 1, 3, 2, 2],  # 4 : [$]
+                           [1, 3, 1, 2, 2, 2],  # 5 : [%]
+                           [1, 2, 2, 2, 1, 3],  # 6 : [&]
+                           [1, 2, 2, 3, 1, 2],  # 7 : [']
+                           [1, 3, 2, 2, 1, 2],  # 8 : [(]
+                           [2, 2, 1, 2, 1, 3],  # 9 : [)]
+                           [2, 2, 1, 3, 1, 2],  # 10 : [*]
+                           [2, 3, 1, 2, 1, 2],  # 11 : [+]
+                           [1, 1, 2, 2, 3, 2],  # 12 : [,]
+                           [1, 2, 2, 1, 3, 2],  # 13 : [-]
+                           [1, 2, 2, 2, 3, 1],  # 14 : [.]
+                           [1, 1, 3, 2, 2, 2],  # 15 : [/]
+                           [1, 2, 3, 1, 2, 2],  # 16 : [0]
+                           [1, 2, 3, 2, 2, 1],  # 17 : [1]
+                           [2, 2, 3, 2, 1, 1],  # 18 : [2]
+                           [2, 2, 1, 1, 3, 2],  # 19 : [3]
+                           [2, 2, 1, 2, 3, 1],  # 20 : [4]
+                           [2, 1, 3, 2, 1, 2],  # 21 : [5]
+                           [2, 2, 3, 1, 1, 2],  # 22 : [6]
+                           [3, 1, 2, 1, 3, 1],  # 23 : [7]
+                           [3, 1, 1, 2, 2, 2],  # 24 : [8]
+                           [3, 2, 1, 1, 2, 2],  # 25 : [9]
+                           [3, 2, 1, 2, 2, 1],  # 26 : [:]
+                           [3, 1, 2, 2, 1, 2],  # 27 : [;]
+                           [3, 2, 2, 1, 1, 2],  # 28 : [<]
+                           [3, 2, 2, 2, 1, 1],  # 29 : [=]
+                           [2, 1, 2, 1, 2, 3],  # 30 : [>]
+                           [2, 1, 2, 3, 2, 1],  # 31 : [?]
+                           [2, 3, 2, 1, 2, 1],  # 32 : [@]
+                           [1, 1, 1, 3, 2, 3],  # 33 : [A]
+                           [1, 3, 1, 1, 2, 3],  # 34 : [B]
+                           [1, 3, 1, 3, 2, 1],  # 35 : [C]
+                           [1, 1, 2, 3, 1, 3],  # 36 : [D]
+                           [1, 3, 2, 1, 1, 3],  # 37 : [E]
+                           [1, 3, 2, 3, 1, 1],  # 38 : [F]
+                           [2, 1, 1, 3, 1, 3],  # 39 : [G]
+                           [2, 3, 1, 1, 1, 3],  # 40 : [H]
+                           [2, 3, 1, 3, 1, 1],  # 41 : [I]
+                           [1, 1, 2, 1, 3, 3],  # 42 : [J]
+                           [1, 1, 2, 3, 3, 1],  # 43 : [K]
+                           [1, 3, 2, 1, 3, 1],  # 44 : [L]
+                           [1, 1, 3, 1, 2, 3],  # 45 : [M]
+                           [1, 1, 3, 3, 2, 1],  # 46 : [N]
+                           [1, 3, 3, 1, 2, 1],  # 47 : [O]
+                           [3, 1, 3, 1, 2, 1],  # 48 : [P]
+                           [2, 1, 1, 3, 3, 1],  # 49 : [Q]
+                           [2, 3, 1, 1, 3, 1],  # 50 : [R]
+                           [2, 1, 3, 1, 1, 3],  # 51 : [S]
+                           [2, 1, 3, 3, 1, 1],  # 52 : [T]
+                           [2, 1, 3, 1, 3, 1],  # 53 : [U]
+                           [3, 1, 1, 1, 2, 3],  # 54 : [V]
+                           [3, 1, 1, 3, 2, 1],  # 55 : [W]
+                           [3, 3, 1, 1, 2, 1],  # 56 : [X]
+                           [3, 1, 2, 1, 1, 3],  # 57 : [Y]
+                           [3, 1, 2, 3, 1, 1],  # 58 : [Z]
+                           [3, 3, 2, 1, 1, 1],  # 59 : [[]
+                           [3, 1, 4, 1, 1, 1],  # 60 : [\]
+                           [2, 2, 1, 4, 1, 1],  # 61 : []]
+                           [4, 3, 1, 1, 1, 1],  # 62 : [^]
+                           [1, 1, 1, 2, 2, 4],  # 63 : [_]
+                           [1, 1, 1, 4, 2, 2],  # 64 : [`]
+                           [1, 2, 1, 1, 2, 4],  # 65 : [a]
+                           [1, 2, 1, 4, 2, 1],  # 66 : [b]
+                           [1, 4, 1, 1, 2, 2],  # 67 : [c]
+                           [1, 4, 1, 2, 2, 1],  # 68 : [d]
+                           [1, 1, 2, 2, 1, 4],  # 69 : [e]
+                           [1, 1, 2, 4, 1, 2],  # 70 : [f]
+                           [1, 2, 2, 1, 1, 4],  # 71 : [g]
+                           [1, 2, 2, 4, 1, 1],  # 72 : [h]
+                           [1, 4, 2, 1, 1, 2],  # 73 : [i]
+                           [1, 4, 2, 2, 1, 1],  # 74 : [j]
+                           [2, 4, 1, 2, 1, 1],  # 75 : [k]
+                           [2, 2, 1, 1, 1, 4],  # 76 : [l]
+                           [4, 1, 3, 1, 1, 1],  # 77 : [m]
+                           [2, 4, 1, 1, 1, 2],  # 78 : [n]
+                           [1, 3, 4, 1, 1, 1],  # 79 : [o]
+                           [1, 1, 1, 2, 4, 2],  # 80 : [p]
+                           [1, 2, 1, 1, 4, 2],  # 81 : [q]
+                           [1, 2, 1, 2, 4, 1],  # 82 : [r]
+                           [1, 1, 4, 2, 1, 2],  # 83 : [s]
+                           [1, 2, 4, 1, 1, 2],  # 84 : [t]
+                           [1, 2, 4, 2, 1, 1],  # 85 : [u]
+                           [4, 1, 1, 2, 1, 2],  # 86 : [v]
+                           [4, 2, 1, 1, 1, 2],  # 87 : [w]
+                           [4, 2, 1, 2, 1, 1],  # 88 : [x]
+                           [2, 1, 2, 1, 4, 1],  # 89 : [y]
+                           [2, 1, 4, 1, 2, 1],  # 90 : [z]
+                           [4, 1, 2, 1, 2, 1],  # 91 : [{]
+                           [1, 1, 1, 1, 4, 3],  # 92 : [|]
+                           [1, 1, 1, 3, 4, 1],  # 93 : [}]
+                           [1, 3, 1, 1, 4, 1],  # 94 : [~]
+                           [1, 1, 4, 1, 1, 3],  # 95 : [DEL]
+                           [1, 1, 4, 3, 1, 1],  # 96 : [FNC3]
+                           [4, 1, 1, 1, 1, 3],  # 97 : [FNC2]
+                           [4, 1, 1, 3, 1, 1],  # 98 : [SHIFT]
+                           [1, 1, 3, 1, 4, 1],  # 99 : [Cswap]
+                           [1, 1, 4, 1, 3, 1],  # 100 : [Bswap]
+                           [3, 1, 1, 1, 4, 1],  # 101 : [Aswap]
+                           [4, 1, 1, 1, 3, 1],  # 102 : [FNC1]
+                           [2, 1, 1, 4, 1, 2],  # 103 : [Astart]
+                           [2, 1, 1, 2, 1, 4],  # 104 : [Bstart]
+                           [2, 1, 1, 2, 3, 2],  # 105 : [Cstart]
+                           [2, 3, 3, 1, 1, 1],  # 106 : [STOP]
+                           [2, 1]]  # 107 : [END BAR]
+        i = 32
+        self.conjunto_c = '0123456789'
+        while i <= 95:  # conjunto de caracteres
+            self.conjunto_abc += chr(i)
+            i += 1
+        self.conjunto_a = self.conjunto_abc
+        self.conjunto_b = self.conjunto_abc
+        i = 0
+        while i <= 31:
+            self.conjunto_abc += chr(i)
+            self.conjunto_a += chr(i)
+            i += 1
+        i = 96
+        while i <= 126:
+            self.conjunto_abc += chr(i)
+            self.conjunto_b += chr(i)
+            i += 1
+        i = 0
+        while i < 107:
+            if i < 96:
+                self.conjunto_de['A'] += chr(i)
+                self.conjunto_de['B'] += chr(i + 32)
+                self.conjunto_para['A'] += chr((i + 64) if i < 32 else (i - 32))
+                self.conjunto_para['B'] += chr(i)
+            else:
+                self.conjunto_de['A'] += chr(i + 104)
+                self.conjunto_de['B'] += chr(i + 104)
+                self.conjunto_para['A'] += chr(i)
+                self.conjunto_para['B'] += chr(i)
+            i += 1
+
+    def header(self):
+        self.cabecalho()
+
+    def cabecalho(self):
+        pass
 
     def retangulo(self, x: int, y: int, l: int, a: int) -> [int, int]:
         self.rect(x, y, l, a)
@@ -33,7 +198,7 @@ class DFePDF(FPDF):
     def quebra_texto(self, texto: str, largura_max: int) -> [str, int]:
         texto = texto.strip()
         if texto == '':
-            return 0
+            return texto, 0
         espaco = self.get_string_width(' ')
         linhas = texto.split('\n')
         texto = ''
@@ -68,7 +233,7 @@ class DFePDF(FPDF):
     # Cria uma caixa de texto com ou sem bordas. Esta função perimite o alinhamento horizontal
     # ou vertical do texto dentro da caixa.
     def caixa_de_texto(self, x: int, y: int, l: int, a: int, texto: str = '', fonte: FontePDF = FontePDF(), alinhamento_v: str = 'T', alinhamento_h: str = 'L',
-                       borda: bool = True, forcar: bool = True, altura_max: int = 0, deslocamento_v: int = 0):
+                       borda: bool = True, forcar: bool = False, altura_max: int = 0, deslocamento_v: int = 0):
         y_ant = y
         y1 = 0
         x1 = 0
@@ -131,3 +296,71 @@ class DFePDF(FPDF):
             if altura_max > 0 and y1 > (y + (altura_max - 1)):
                 break
         return (y1 - y) - incremento_y
+
+    def codigo_barras_128(self, x: int, y: int, codigo: str, l: int, a: int):
+        guia_a = ''
+        guia_b = ''
+        guia_c = ''
+        i = 0
+        while i < len(codigo):
+            agora = codigo[i]
+            guia_a += 'O' if agora in self.conjunto_a else 'N'
+            guia_b += 'O' if agora in self.conjunto_b else 'N'
+            guia_c += 'O' if agora in self.conjunto_c else 'N'
+            i += 1
+        s_codigo_pequeno = 'OOOO'
+        i_codigo_pequeno = 4
+        codificado = ''
+        while codigo > '':
+            i = guia_c.find(s_codigo_pequeno)
+            if i >= 0:
+                guia_a = guia_a[:i] + 'N' + guia_a[i:]
+                guia_b = guia_b[:i] + 'N' + guia_b[i:]
+            if guia_c[:i_codigo_pequeno] == s_codigo_pequeno:
+                codificado += chr(self.conjunto_troca['C'] if codificado > '' else self.conjunto_inicio['C'])
+                feito = guia_c.find('N')
+                if feito < 0:
+                    feito = len(guia_c)
+                if fmod(feito, 2) == 1:
+                    feito -= 1
+                i = 0
+                while i < feito:
+                    codificado += chr(int(codigo[i:i + 2]))
+                    i += 2
+                grupo = 'C'
+            else:
+                feito_a = guia_a.find('N')
+                if feito_a < 0:
+                    feito_a = len(guia_a)
+                feito_b = guia_b.find('N')
+                if feito_b < 0:
+                    feito_b = len(guia_b)
+                if feito_a < feito_b:
+                    feito = feito_b
+                    grupo = 'B'
+                else:
+                    feito = feito_a
+                    grupo = 'A'
+                codificado += chr(self.conjunto_troca[grupo] if codificado > '' else self.conjunto_inicio[grupo])
+                codificado += strtr(codigo[0: feito], self.conjunto_de[grupo], self.conjunto_para[grupo])
+            codigo = codigo[feito:]
+            guia_a = guia_a[feito:]
+            guia_b = guia_b[feito:]
+            guia_c = guia_c[feito:]
+        checagem = ord(codificado[0])
+        i = 0
+        while i < len(codificado):
+            checagem += (ord(codificado[i]) * i)
+            i += 1
+        checagem %= 103
+        codificado += chr(checagem) + chr(106) + chr(107)
+        modulo = l / ((len(codificado) * 11) - 8)
+        i = 0
+        while i < len(codificado):
+            coordenadas = self.tabela_128[ord(codificado[i])]
+            j = 0
+            while j < len(coordenadas):
+                self.rect(x, y, coordenadas[j] * modulo, a, "F")
+                x += (coordenadas[j] + coordenadas[j+1]) * modulo
+                j += 2
+            i += 1
--- documento/nfe.py
+++ documento/nfe.py
@@ -2,10 +2,28 @@ from collections import OrderedDict
 
 from xmltodict import parse as x2d_parse
 
-from documento.DFePDF import FontePDF
-from .DFePDF import DFePDF
-from .dfe import InfNFe
-from .formatos import f_dma, f_moeda, f_int_milhar
+from .DFePDF import FontePDF, DFePDF
+from .dfe import InfNFe, Endereco
+from .formatos import f_dma, f_moeda, f_int_milhar, f_cep, f_fone, f_espaco_a_cada
+
+
+def construir_endereco(obj_end: Endereco, com_endereco: bool = True, com_cep: bool = False, com_cidade: bool = True, com_fone: bool = False) -> str:
+    retorno = ''
+
+    def se_tem():
+        if len(retorno) > 0:
+            return ' - '
+        return ''
+
+    if com_endereco:
+        retorno += f'{obj_end.logradouro}, {obj_end.numero} - {obj_end.complemento}, {obj_end.bairro}'
+    if com_cep:
+        retorno += se_tem() + f'{f_cep(obj_end.cep)}'
+    if com_cidade:
+        retorno += se_tem() + f'{obj_end.municipio}/{obj_end.uf}'
+    if com_fone:
+        retorno += se_tem() + f'FONE: {f_fone(obj_end.telefone)}'
+    return retorno
 
 
 class NFe(object):
@@ -43,29 +61,81 @@ class DANFe(DFePDF):
         self.nfe = nfe.infNFe
 
     def canhoto(self) -> int:
-        destinatario = f'{self.nfe.destinatario.razao_social} - {self.nfe.destinatario.endereco.logradouro}, {self.nfe.destinatario.endereco.numero} - ' \
-                       f'{self.nfe.destinatario.endereco.complemento},  {self.nfe.destinatario.endereco.bairro} - {self.nfe.destinatario.endereco.municipio}/' \
-                       f' {self.nfe.destinatario.endereco.uf}'
+        destinatario = f'{self.nfe.destinatario.razao_social} - {construir_endereco(self.nfe.destinatario.endereco)}'
         conteudo = f'RECEBEMOS DE {self.nfe.emitente.razao_social} OS PRODUTOS E/OU SERVIÇOS CONSTANTES DA NOTA FISCAL ELETRÔNICA INDICADA AO LADO. ' \
                    f'EMISSÃO: {f_dma(self.nfe.ide.data_emissao)} VALOR TOTAL: {f_moeda(self.nfe.total.icms.valor_nf)} DESTINATÁRIO: {destinatario}'
         fonte = FontePDF(tamanho=7)
-        largura = round(self.max_largura * 0.81)
-        x = self.x + largura
+        x = largura = round((self.largura_max - self.x) * 0.81)
         y = self.y + 10
         self.caixa_de_texto(self.x, self.y, largura, 10, conteudo, fonte, forcar=False)
-        div_e = self.max_largura - x
+        div_d = self.largura_max - (self.x + x)
         fonte.tamanho = 13
         fonte.estilo = 'B'
-        self.caixa_de_texto(x, self.y, self.max_largura - x, 20, f'NF-e\nNº {f_int_milhar(self.nfe.ide.nf)}\n Série  {self.nfe.ide.serie}', fonte, forcar=False,
-                            alinhamento_v='C', alinhamento_h='C')
+        self.caixa_de_texto(self.x + x, self.y, div_d, 20, f'NF-e\nNº {f_int_milhar(self.nfe.ide.nf)}\n SÉRIE {self.nfe.ide.serie}', fonte,
+                            forcar=False, alinhamento_v='C', alinhamento_h='C')
         fonte.tamanho = 7
         fonte.estilo = ''
-        self.caixa_de_texto(self.x, y, largura * 0.2, 10, 'DATA DE RECEBIMENTO', fonte, forcar=False)
-        x = self.x + (largura * 0.2)
-        self.caixa_de_texto(x, y, self.max_largura - x - div_e, 10, 'IDENTIFICAÇÃO E ASSINATURA DO RECEBEDOR', fonte, forcar=False)
+        x = round(largura * 0.2)
+        self.caixa_de_texto(self.x, y, x, 10, 'DATA DE RECEBIMENTO', fonte, forcar=False)
+        self.caixa_de_texto(self.x + x, y, self.largura_max - x - div_d - self.x, 10, 'IDENTIFICAÇÃO E ASSINATURA DO RECEBEDOR', fonte, forcar=False)
         y = self.y + 22
-        self.dashed_line(self.x, y, self.max_largura, y)
+        self.dashed_line(self.x, y, self.largura_max, y)
         return y + 2
 
-    def header(self):
-        self.canhoto()
+    def cabecalho(self):
+        a_cabecalho = 33
+        posicao_y = y = self.canhoto()
+        posicao_x = x = round((self.largura_max - self.x) * 0.40)
+        x2 = round((self.largura_max - self.x) * 0.20)
+        largura_ult_caixa = self.largura_max - x - x2 - self.x
+        fonte = FontePDF(tamanho=6)
+        self.caixa_de_texto(self.x, y, x, a_cabecalho, fonte=fonte)
+        self.caixa_de_texto(self.x + x, y, x2, a_cabecalho)
+        self.caixa_de_texto(self.x + x + x2, y, largura_ult_caixa, a_cabecalho)
+        tamanho = self.font_size
+        posicao_y = posicao_y + self.caixa_de_texto(self.x, y, x, tamanho, "IDENTIFICAÇÃO DO EMITENTE", fonte, 'T', 'C', False) + 2
+        fonte.tamanho = 12
+        fonte.estilo = 'B'
+        posicao_y = posicao_y + self.caixa_de_texto(self.x, posicao_y, x, 12, f'{self.nfe.emitente.razao_social} - {self.nfe.emitente.fantasia}', fonte, 'T',
+                                                    'C', False) + 2
+        fonte.tamanho = 8
+        fonte.estilo = ''
+        ender = f'{construir_endereco(self.nfe.emitente.endereco, com_cep=True, com_cidade=False)}\n' \
+                f'{construir_endereco(self.nfe.emitente.endereco, False, False, True, True)}'
+        self.caixa_de_texto(self.x, posicao_y, x, 12, ender, fonte, 'T', 'C', False)
+        fonte.tamanho = 12
+        fonte.estilo = 'B'
+        posicao_x += self.x
+        posicao_y = y + self.caixa_de_texto(posicao_x, y, x2, 12, 'DANFE', fonte, 'T', 'C', False)
+        fonte.tamanho = 10
+        fonte.estilo = ''
+        entrada = '1 - ENTRADA'
+        posicao_y = posicao_y + self.caixa_de_texto(posicao_x, posicao_y, x2, 12, 'Documento Auxiliar da Nota Fiscal Eletrônica', fonte, 'T', 'C', False) + 2
+        a_quad = self.caixa_de_texto(posicao_x + 2, posicao_y, x2, 12, f'{entrada}\n2 - SAÍDA', fonte, 'T', 'L', False)
+        fonte.tamanho = 12
+        fonte.estilo = 'B'
+        posicao_y = posicao_y + self.caixa_de_texto(posicao_x + self.get_string_width(entrada) + 7, posicao_y , self.get_string_width('0000'), a_quad,
+                                                    str(self.nfe.ide.tipo_nf), fonte, 'C', 'C') + 2
+        fonte.tamanho = 10
+        fonte.estilo = 'B'
+        posicao_y = posicao_y + self.caixa_de_texto(posicao_x, posicao_y, x2, 12, f'{f_int_milhar(self.nfe.ide.nf)}', fonte, 'T', 'C', False)
+        posicao_y = posicao_y + self.caixa_de_texto(posicao_x, posicao_y, x2, 12, f'SÉRIE {self.nfe.ide.serie}', fonte, 'T', 'C', False)
+        self.caixa_de_texto(posicao_x + 2, posicao_y, x2, 12, f'FOLHA {self.page_no()}/{{nb}}', fonte, 'T', 'C', False)
+        posicao_x += x2
+        b_largura = largura_ult_caixa - 4
+        b_altura = 12
+        chave_nfe = self.nfe.id.replace('NFe', '')
+        self.codigo_barras_128(posicao_x + 2, y + 2, chave_nfe, b_largura, b_altura)
+        fonte.tamanho = 6
+        fonte.estilo = ''
+        posicao_y = y + 16
+        self.caixa_de_texto(posicao_x, posicao_y, largura_ult_caixa, 8, '', fonte, 'T', 'L')
+        posicao_y = posicao_y + self.caixa_de_texto(posicao_x, posicao_y, largura_ult_caixa, 8, f'CHAVE DE ACESSO', fonte, 'T', 'L', False) + 1
+        fonte.tamanho = 9
+        fonte.estilo = 'B'
+        posicao_y = posicao_y + self.caixa_de_texto(posicao_x, posicao_y, largura_ult_caixa, 8, f_espaco_a_cada(chave_nfe, 4), fonte, 'T', 'L', False) + 3
+        fonte.tamanho = 8
+        fonte.estilo = ''
+        posicao_y = posicao_y + self.caixa_de_texto(posicao_x, posicao_y, largura_ult_caixa, 8,
+                                                    'Consulta de autenticidade no portal nacional da NF-e \nwww.nfe.fazenda.gov.br/portal ou no site da Sefaz '
+                                                    'Autorizadora', fonte, 'T', 'C', False)
